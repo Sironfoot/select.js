70389a2e4696e86c36471ff4e5cd37c8fc367648
Dominic Pettifer <sironfoot@gmail.com>
2013-08-14 09:27:22 +0100

    Unit tests for  function

---
+++ test/where.js
@@ -0,0 +1,101 @@
+var assert = require('assert');
+var data = require('./_data');
+require('../');
+
+var customers = data.customers;
+var scores = data.scores;
+
+describe('#where()', function() {
+    it('should find a simple match', function() {
+        var result = customers.$_where({ name: 'John Smith' });
+        
+        assert.ok(Array.isArray(result));
+        assert.ok(result.length > 0);
+        
+        result.forEach(function(customer) {
+            assert.equal(customer.name, 'John Smith');
+        });
+    });
+    
+    it('should support Greater Than searches', function() {
+        var result = scores.$_where({ score: { $gt: 5 }});
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 5);
+        
+        result.forEach(function(score) {
+            assert.ok(score.score > 5);
+        });
+    });
+    
+    it('should support Greater Than Equal searches', function() {
+        var result = scores.$_where({ score: { $gte: 5 }});
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 6);
+        
+        result.forEach(function(score) {
+            assert.ok(score.score >= 5);
+        });
+    });
+    
+    it('should support Less Than searches', function() {
+        var result = scores.$_where({ score: { $lt: 6 }});
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 5);
+        
+        result.forEach(function(score) {
+            assert.ok(score.score < 6);
+        });
+    });
+    
+    it('should support Less Than Equal searches', function() {
+        var result = scores.$_where({ score: { $lte: 6 }});
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 6);
+        
+        result.forEach(function(score) {
+            assert.ok(score.score <= 6);
+        });
+    });
+    
+    it('should support Range searches', function() {
+        var result = scores.$_where({ score: { $gt: 4, $lt: 8 }});
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 3);
+        
+        result.forEach(function(score) {
+            assert.ok(score.score > 4);
+            assert.ok(score.score < 8);
+        });
+    });
+    
+    it('should support In searches', function() {
+        var result = scores.$_where({ score: { $in: [ 1, 3, 5, 7, 9 ] }});
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 5);
+        
+        result.forEach(function(score) {
+            assert.ok(
+                score.score === 1 ||
+                score.score === 3 ||
+                score.score === 5 ||
+                score.score === 7 ||
+                score.score === 9);
+        });
+    });
+    
+    it('should support custom function searches', function() {
+        var result = scores.$_where(function(score) {
+            return score.name === 'Number 1';
+        });
+        
+        assert.ok(Array.isArray(result));
+        assert.equal(result.length, 1)
+        assert.equal(result[0].name, 'Number 1');
+    });
+});
