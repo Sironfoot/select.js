675c07f239b487e5368c3e86932ff2be2810098b
Dominic Pettifer <sironfoot@gmail.com>
2013-08-14 20:28:32 +0100

    Unit tests for any, all, first and last functions

---
+++ test/anyAll.js
@@ -0,0 +1,67 @@
+'use strict';
+
+var assert = require('assert');
+var data = require('./_data');
+require('../');
+
+var customers = data.customers;
+var scores = data.scores;
+var people = data.people;
+
+describe('#any()', function() {
+    it('should return true on match', function() {
+        var hasCustomer = customers.$_any({ name: 'John Smith' });
+        assert.ok(hasCustomer);
+        
+        var hasScore = scores.$_any({ score: { $gt: 5 }});
+        assert.ok(hasScore);
+        
+        var hasPerson = people.$_any(function(person) {
+            return person.name === 'AA';
+        });
+        assert.ok(hasPerson);
+    });
+    
+    it('should return false when no match', function() {
+        var hasCustomer = customers.$_any({ name: 'George Bush' });
+        assert.ok(!hasCustomer);
+        
+        var hasScore = scores.$_any({ score: { $gt: 100 }});
+        assert.ok(!hasScore);
+        
+        var hasPerson = people.$_any(function(person) {
+            return person.name === 'XX';
+        });
+        assert.ok(!hasPerson);
+    });
+    
+    it('should return true/false with no query, depending on whether collection contains any items', function() {
+        var hasCustomers = customers.$_any();
+        assert.ok(hasCustomers);
+        
+        var emptyArray = [];
+        var hasItems = emptyArray.$_any();
+        assert.ok(!hasItems);
+    });
+});
+
+describe('#all()', function() {
+    it('should return true on all match', function() {
+        var allScoresMoreThanZero = scores.$_all({ score: { $gt: 0 }});
+        assert.ok(allScoressMoreThanZero);
+    });
+    
+    it('should return false on not all matching query', function() {
+        var allScoresLessThanNine = scores.$_all({ score: { $lt: 9 }});
+        assert.ok(!allScoresLessThanNine);
+    });
+    
+    it('should return true/false with no query, depending on whether collection contains any items', function() {
+        var hasCustomers = customers.$_all();
+        assert.ok(hasCustomers);
+        
+        var emptyArray = [];
+        var hasItems = emptyArray.$_all();
+        assert.ok(!hasItems);
+    });
+});
---
+++ test/firstLast.js
@@ -0,0 +1,55 @@
+'use strict';
+
+var assert = require('assert');
+var data = require('./_data');
+require('../');
+
+var customers = data.customers;
+var scores = data.scores;
+var people = data.people;
+
+describe('#first()', function() {
+    it('should return the first item in an array', function() {
+        var firstScore = scores.$_first();
+        
+        assert.ok(firstScore);
+        assert.equal(firstScore, scores[0]);
+    });
+    
+    it('should return null if the array is empty', function() {
+        var emptyArray = [];
+        var firstItem = emptyArray.$_first();
+        
+        assert.ok(firstItems === null);
+    });
+    
+    it('should allow searches', function() {
+        var score = scores.$_first({ score: 1 });
+        
+        assert.ok(score);
+        assert.equal(score.score, 1);
+    });
+});
+
+describe('#last()', function() {
+    it('should return the last item in an array', function() {
+        var lastScore = scores.$_last();
+        
+        assert.ok(lastScore);
+        assert.equal(lastScore, scores[scores.length - 1]);
+    });
+    
+    it('should return null if the array is empty', function() {
+        var emptyArray = [];
+        var lastItem = emptyArray.$_last();
+        
+        assert.ok(lastItem === null);
+    });
+    
+    it('should allow searches', function() {
+        var score = scores.$_last({ score: 1 });
+        
+        assert.ok(score);
+        assert.equal(score.score, 1);
+    });
+});
